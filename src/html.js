69fc56519cdb4cf245fbbbe0ec2e838c06680542
chirp <ch1rp@protonmail.com>
2022-02-19 02:10:26 +0000

    prototype html parser (incomplete)

---
+++ src/html.js
@@ -0,0 +1,54 @@
+// Global
+
+const tagRegex = /<(.*?)>/g;
+
+// Parse HTMl elements into JSON
+
+function _recurse(elements, _fallback) {
+	const first = elements.splice(0, 1)[0];
+
+	if (!first || first.tagRaw.startsWith("</"))
+		return _fallback || null;
+
+	while (elements.length > 0) {
+		const second = elements.splice(0, 1)[0];
+		if (first.tagRaw.startsWith("<input") || second.tagRaw.startsWith("</"))
+			break;
+		first.children.push(_recurse(elements, second));
+	}
+	
+	return first;
+}
+
+function _parseElements(elements) {
+	const list = [];
+
+	while (elements.length > 0) {
+		const element = _recurse(elements);
+		if (element != null)
+			list.push(element);
+	}
+
+	return list;
+}
+
+function parseHtml(content) {
+	const elements = [];
+
+	let exec;
+	while ((exec = tagRegex.exec(content)) !== null) {
+		elements.push({
+			tagRaw: exec[0],
+			index: exec.index,
+			children: []
+		});
+	}
+
+	const res = JSON.stringify(_parseElements(elements), null, 4);
+
+	return res;
+}
+
+// Export
+
+module.exports = {parseHtml};
---
+++ src/parse.js
@@ -0,0 +1,30 @@
+// Global
+
+const {parseHtml} = require("./html");
+
+// Parse HTML
+
+function _charData(content) {
+	const split = content.split("\n");
+
+	let found = false;
+	for (const index in split) {
+		const line = split[index];
+		if (found) {
+			if (line.startsWith("<div"))
+				return parseHtml(line);
+		} else if (line.includes("character__content selected")) {
+			found = true;
+		}
+	}
+}
+
+// Parse character
+
+function parse(content) {
+	return _charData(content);
+}
+
+// Export
+
+module.exports = parse;
